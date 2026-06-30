ea6601e27a7649c5d0d7e940eb1fb8947af16e39
KARAN RAJBHAR <karanrajbhar.dev@gmail.com>
2026-06-30 21:26:51 +0530

    Time: 3 ms (76.73%), Space: 9.7 MB (66.76%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string repeated = a;
+        int count = 1;
+    
+        while (repeated.length() < b.length()) {
+            repeated += a;
+            count++;
+        }
+        
+        if (repeated.find(b) != string::npos) {
+            return count;
+        }
+        
+        repeated += a;
+        if (repeated.find(b) != string::npos) {
+            return count + 1;
+        }
+        
+        return -1;
+    }
+};
