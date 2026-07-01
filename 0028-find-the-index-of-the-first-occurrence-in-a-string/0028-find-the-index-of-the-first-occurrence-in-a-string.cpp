b5d740d3e9dc7e3a58b393463153ff1a64e69dd1
KARAN RAJBHAR <karanrajbhar.dev@gmail.com>
2026-07-01 19:29:33 +0530

    Time: 3 ms (20.09%), Space: 9.8 MB (6.37%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,39 +1,41 @@
 class Solution {
 private:
-    vector<int> zFunction(const string& s) {
-        int n = s.length();
-        vector<int> z(n, 0);
-        int l = 0, r = 0;
-        
-        for (int i = 1; i < n; i++) {
-            if (i <= r) {
-                z[i] = min(r - i + 1, z[i - l]);
-            }
-            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-                z[i]++;
-            }
-            if (i + z[i] - 1 > r) {
-                l = i;
-                r = i + z[i] - 1;
+    vector<int> computeLPS(string s) {
+        int n = s.size();
+        vector<int> LPS(n, 0);
+
+        int i = 1, j = 0;
+        while (i < n) {
+            if (s[i] == s[j]) {
+                LPS[i] = j + 1;
+                j++, i++;
+            } else {
+                while (j > 0 && s[i] != s[j]) {
+                    j = LPS[j - 1];
+                }
+                if (s[i] == s[j]) {
+                    LPS[i] = j + 1;
+                    j++;
+                }
+                i = i + 1;
             }
         }
-        return z;
+        return LPS;
     }
 
 public:
     int strStr(string haystack, string needle) {
-        if (needle.empty()) return 0; 
+        string s = needle + '$' + haystack;
+        vector<int> LPS = computeLPS(s);
 
-        string combined = needle + "$" + haystack; 
-        vector<int> z = zFunction(combined);
-        
-        int needleLen = needle.size();
-        for (int i = needleLen + 1; i < (int)combined.size(); i++) {
-            if (z[i] == needleLen) {
-                return i - needleLen - 1; 
+        int n = needle.size(), m = haystack.size();
+
+        for (int i = n + 1; i < s.size(); i++) {
+            if (LPS[i] == n) {
+                return (i - n - 1) - n + 1;
             }
         }
-        
-        return -1; 
+
+        return -1;
     }
 };
