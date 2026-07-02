abdf9b55b074ab96ee6f11ce368ddc2b3436be54
KARAN RAJBHAR <karanrajbhar.dev@gmail.com>
2026-07-02 20:02:39 +0530

    Time: 3 ms (81.65%), Space: 9.1 MB (94.04%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1) return "1";
+
+        string prev = countAndSay(n-1);
+        int len = prev.length();
+
+        string ans = "";
+        int count = 1;
+
+        for(int i = 1; i < len; i++){
+            if(prev[i] == prev[i-1]) count++;
+            else{
+                ans.push_back('0' + count);
+                ans.push_back(prev[i-1]);
+                count = 1;
+            }
+        }
+
+        ans.push_back('0' + count);
+        ans.push_back(prev[len-1]);
+
+        return ans;
+        
+    }
+};
